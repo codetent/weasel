40d3caf098c085748b34f3b15970239cf199bdff
codetent <22278777+codetent@users.noreply.github.com>
2021-12-02 02:26:03 +0100

    Improve help

--- cmd/root.go
+++ cmd/root.go
@@ -29,14 +29,8 @@ var (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "weasel",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:  "weasel",
+	Long: "Tool for managing WSL distributions like docker containers.",
 	CompletionOptions: cobra.CompletionOptions{
 		DisableDefaultCmd: true,
 	},
