6088fdf0fa813c335a5bd9f1a9d1193e322964df
codetent <22278777+codetent@users.noreply.github.com>
2021-12-02 02:25:11 +0100

    Improve rm

--- cmd/rm.go
+++ cmd/rm.go
@@ -19,43 +19,30 @@ import (
 	"github.com/codetent/weasel/pkg/weasel/store"
 	"github.com/codetent/weasel/pkg/weasel/wsl"
 	"github.com/spf13/cobra"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // rmCmd represents the rm command
 var rmCmd = &cobra.Command{
 	Use:   "rm",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.ExactArgs(1),
+	Short: "Remove instance",
+	Long:  "Remove an instance that is in any state by its id.",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		instanceId := args[0]
 		err := store.UnregisterInstance(instanceId)
 		if err != nil {
-			panic(err)
+			log.Errorf("Error unregistering instance: %v", err)
 		}
 
 		err = wsl.Unregister(instanceId)
 		if err != nil {
-			panic(err)
+			log.Errorf("Error unloading instance: %v", err)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(rmCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// rmCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// rmCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
