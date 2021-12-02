b8901d58bd4f396741267b81ee64a8077cca6641
codetent <22278777+codetent@users.noreply.github.com>
2021-12-02 02:25:54 +0100

    Add distribution handling

--- pkg/weasel/store/registry.go
+++ pkg/weasel/store/registry.go
@@ -12,6 +12,10 @@ const (
 	BucketInstances     string = "instances"
 )
 
+type RegisteredDistribution struct {
+	Id string
+}
+
 type RegisteredInstance struct {
 	Id           string
 	Distribution string
@@ -64,6 +68,24 @@ func RegisterDistribution(id string, path string) error {
 	return nil
 }
 
+func UnregisterDistribution(id string) error {
+	db, err := OpenDatabase()
+	if err != nil {
+		return fmt.Errorf("UnregisterDistribution: OpenDatabase(): %v", err)
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BucketDistributions))
+		return bucket.Delete([]byte(id))
+	})
+	if err != nil {
+		return fmt.Errorf("UnregisterDistribution: Update(): %v", err)
+	}
+
+	return nil
+}
+
 func GetRegisteredDistribution(id string) (string, error) {
 	db, err := OpenDatabase()
 	if err != nil {
@@ -80,6 +102,30 @@ func GetRegisteredDistribution(id string) (string, error) {
 	bucket := rx.Bucket([]byte(BucketDistributions))
 	raw := bucket.Get([]byte(id))
 	return string(raw), nil
+}
+
+func GetRegisteredDistributions() ([]RegisteredDistribution, error) {
+	db, err := OpenDatabase()
+	if err != nil {
+		return nil, fmt.Errorf("GetRegisteredDistribution: OpenDatabase(): %v", err)
+	}
+	defer db.Close()
+
+	rx, err := db.Begin(false)
+	if err != nil {
+		return nil, fmt.Errorf("GetRegisteredDistributions: Begin(): %v", err)
+	}
+	defer rx.Rollback()
+
+	bucket := rx.Bucket([]byte(BucketDistributions))
+	cursor := bucket.Cursor()
+	var dists []RegisteredDistribution
+
+	for id, _ := cursor.First(); id != nil; id, _ = cursor.Next() {
+		dists = append(dists, RegisteredDistribution{string(id)})
+	}
+
+	return dists, nil
 }
 
 func RegisterInstance(id string, dist string) error {
