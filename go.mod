b39d96077c397bfe894cd072c0e33e6d357e8b68
codetent <22278777+codetent@users.noreply.github.com>
2021-12-04 02:29:03 +0100

    Add some e2e tests

--- go.mod
+++ go.mod
@@ -6,14 +6,19 @@ require (
 	github.com/containerd/containerd v1.5.8 // indirect
 	github.com/docker/docker v20.10.11+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0
 	github.com/mholt/archiver/v3 v3.5.1
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6
 	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/onsi/ginkgo/v2 v2.0.0-rc2
+	github.com/onsi/gomega v1.17.0
 	github.com/opencontainers/image-spec v1.0.2 // indirect
-	github.com/rs/xid v1.3.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
 	go.etcd.io/bbolt v1.3.6
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
---
+++ tests/e2e/run_test.go
@@ -0,0 +1,58 @@
+package e2e_test
+
+import (
+	"os/exec"
+
+	"github.com/codetent/weasel/pkg/weasel/store"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Run", func() {
+	var weaselPath string
+
+	BeforeEach(func() {
+		var err error
+
+		weaselPath, err = gexec.Build("github.com/codetent/weasel")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		err := store.UnregisterDistribution("test_hub_image")
+		Expect(err).NotTo(HaveOccurred())
+
+		gexec.CleanupBuildArtifacts()
+	})
+
+	Describe("Run instance", func() {
+		It("from existing distribution", func() {
+			cmd := exec.Command(weaselPath, "build", "--tag", "test_hub_image", "hub:busybox")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+
+			session.Wait(30)
+			Expect(session).Should(gexec.Exit(0))
+
+			cmd = exec.Command(weaselPath, "run", "test_hub_image", "echo", "works!")
+			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+
+			session.Wait(30)
+			Expect(session).Should(gexec.Exit(0))
+			Expect(session.Out).Should(gbytes.Say("works!"))
+		})
+
+		It("from non-existing distribution", func() {
+			cmd := exec.Command(weaselPath, "run", "_", "echo", "works!")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+
+			session.Wait(30)
+			Expect(session).Should(gexec.Exit(1))
+			Expect(session.Out).Should(gbytes.Say(".* Distribution with id _ not found"))
+		})
+	})
+})
